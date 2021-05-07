696cd836adf27d7bb931fb6d519ebaf6d4da6d9f
Aboba <timofeevnik41@gmail.com>
2021-05-07 18:17:14 +0300

    Перекат на джанго

---
+++ projectoring/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for projectoring project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectoring.settings')
+
+application = get_asgi_application()
