900c514e3976cc1d9314a427dbaf8e2cf6e7d8da
Aboba <timofeevnik41@gmail.com>
2021-05-07 16:59:04 +0300

    App structure v2

--- application/database/database_worker.py
+++ application/database/database_worker.py
@@ -1,6 +1,8 @@
 class DatabaseBase:
+    """Abstract class"""
     pass
 
 class DatabaseWorker(DatabaseBase):
+    """Main database worker class"""
     def __init__(self) -> None:
         super().__init__()
--- application/utils/utils.py
+++ application/utils/utils.py
@@ -1,7 +1,9 @@
 class UtilsBase:
+    """Abstract class"""
     pass
 
 class Utils(UtilsBase):
+    """Main utils class"""
     def __init__(self) -> None:
         super().__init__()
 
