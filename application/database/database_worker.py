e9ea3c8be35d2aba8e5ef66eaf91e949a9b7d719
Aboba <timofeevnik41@gmail.com>
2021-05-07 16:58:33 +0300

    App structure

---
+++ application/database/database_worker.py
@@ -0,0 +1,6 @@
+class DatabaseBase:
+    pass
+
+class DatabaseWorker(DatabaseBase):
+    def __init__(self) -> None:
+        super().__init__()
---
+++ application/utils/utils.py
@@ -0,0 +1,9 @@
+class UtilsBase:
+    pass
+
+class Utils(UtilsBase):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def method():
+        pass
