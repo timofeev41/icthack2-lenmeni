3eb1f0a0a7cef0fdd104be32329967810887e606
Aboba <timofeevnik41@gmail.com>
2021-05-07 19:01:20 +0300

    First model for tasks

--- prodtoring/admin.py
+++ prodtoring/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Tasks
+
 # Register your models here.
+class TasksAdmin(admin.ModelAdmin):
+    fields = ['title', 'status', "progress", "completedate"]
+
+admin.site.register(Tasks, TasksAdmin)
