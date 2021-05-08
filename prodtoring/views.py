2180fce154e27b19014341126d196a9dd8cb5788
Lev <tikhonov.lev0@gmail.com>
2021-05-08 15:54:18 +0300

    add templates

--- prodtoring/views.py
+++ prodtoring/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import FormView
 
-# Create your views here.
+# Вариант регистрации на базе класса FormView
+class MyRegisterFormView(FormView):
+    # Указажем какую форму мы будем использовать для регистрации наших пользователей, в нашем случае
+    # это UserCreationForm - стандартный класс Django унаследованный
+    form_class = UserCreationForm
 
+    # Ссылка, на которую будет перенаправляться пользователь в случае успешной регистрации.
+    # В данном случае указана ссылка на страницу входа для зарегистрированных пользователей.
+    success_url = "/login/"
+
+    # Шаблон, который будет использоваться при отображении представления.
+    template_name = "register.html"
+
+    def form_valid(self, form):
+        form.save()
+        # Функция super( тип [ , объект или тип ] ) 
+        # Возвратите объект прокси, который делегирует вызовы метода родительскому или родственному классу типа .
+        return super(MyRegisterFormView, self).form_valid(form)
+
+    def form_invalid(self, form):
+        return super(MyRegisterFormView, self).form_invalid(form)
--- projectoring/urls.py
+++ projectoring/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', )
+    path('accounts/register/', MyRegisterFormView.as_view(), name="register"),
 ]
